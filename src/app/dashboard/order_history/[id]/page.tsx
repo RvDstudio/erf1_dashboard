8a3e3e39c63c759472f65b2d991167daea6acb63
Reinier <39220977+RvDstudio@users.noreply.github.com>
2024-10-04 08:38:12 +0200

    Summary

--- src/app/dashboard/order_history/[id]/page.tsx
+++ src/app/dashboard/order_history/[id]/page.tsx
@@ -17,43 +17,51 @@ export default async function OrderDetails({ params }: { params: { id: string } 
 
   return (
     <div className="p-6">
-      <h1 className="text-xl font-medium mb-4 text-[#374c69]">Bestelgegevens</h1>
-      <p className="text-lg">
-        <strong className="text-[#374c69]">Order ID:</strong> {order.id}
+      <h1 className="text-xl font-medium mb-4 text-[#888888]">Bestelgegevens</h1>
+      <p className="text-lg text-[#374C69]">
+        <strong className="text-[#888888]">Order ID:</strong> {order.id}
       </p>
-      <p className="text-[#374c69]">
+      <p className="text-[#888888]">
         <strong>Totale Prijs:</strong> €{order.total_price}
       </p>
-      <p className="text-[#374c69]">
+      <p className="text-[#888888]">
         <strong>Status:</strong> {order.status}
       </p>
 
-      <h2 className="text-[#374c69] font-medium mt-3 mb-4">Producten in deze order</h2>
+      <h2 className="text-[#888888] font-medium mt-3 mb-4">Producten in deze order</h2>
       {orderItems.length > 0 ? (
-        <table className="min-w-full border-collapse block md:table">
+        <table className="min-w-full dark:text-[#888888]  border-collapse block md:table">
           <thead className="block md:table-header-group">
-            <tr className="border border-gray-300 md:table-row absolute -top-full md:relative">
-              <th className="bg-gray-100 p-2 text-left font-medium md:border md:border-gray-300">Afbeelding</th>
-              <th className="bg-gray-100 p-2 text-left font-medium md:border md:border-gray-300">Product Naam</th>
-              <th className="bg-gray-100 p-2 text-left font-medium md:border md:border-gray-300">Aantal</th>
-              <th className="bg-gray-100 p-2 text-left font-medium md:border md:border-gray-300">Beschrijving</th>
+            <tr className="border border-gray-300 dark:border-[#2e2e2e] md:table-row absolute -top-full md:relative">
+              <th className="bg-gray-100 dark:bg-[#252525] p-2 text-left text-[#888888] font-medium md:border md:border-gray-300 dark:border-[#2e2e2e]">
+                Afbeelding
+              </th>
+              <th className="bg-gray-100 dark:bg-[#252525] p-2 text-left font-medium md:border md:dark:border-[#2e2e2e]">
+                Product Naam
+              </th>
+              <th className="bg-gray-100 dark:bg-[#252525] p-2 text-left font-medium md:border md:dark:border-[#2e2e2e]">
+                Aantal
+              </th>
+              <th className="bg-gray-100 dark:bg-[#252525] p-2 text-left font-medium md:border md:dark:border-[#2e2e2e]">
+                Beschrijving
+              </th>
             </tr>
           </thead>
           <tbody className="block md:table-row-group">
             {orderItems.map((item) => (
-              <tr key={item.id} className="border border-gray-300 md:table-row">
+              <tr key={item.id} className="border dark:border-[#2e2e2e] md:table-row">
                 {/* Render the product image */}
-                <td className="p-2 md:border md:border-gray-300 block md:table-cell">
+                <td className="p-2 md:border md:dark:border-[#2e2e2e] block md:table-cell">
                   {item.image_url && (
                     <img src={item.image_url} alt={item.product_name} className="w-20 h-20 object-cover rounded-lg" />
                   )}
                 </td>
                 {/* Product name */}
-                <td className="p-2 md:border md:border-gray-300 block md:table-cell">{item.product_name}</td>
+                <td className="p-2 md:border md:dark:border-[#2e2e2e] block md:table-cell">{item.product_name}</td>
                 {/* Quantity */}
-                <td className="p-2 md:border md:border-gray-300 block md:table-cell">{item.quantity}</td>
+                <td className="p-2 md:border md:dark:border-[#2e2e2e] block md:table-cell">{item.quantity}</td>
                 {/* Description */}
-                <td className="p-2 md:border md:border-gray-300 block md:table-cell">
+                <td className="p-2 md:border md:dark:border-[#2e2e2e] block md:table-cell">
                   <div dangerouslySetInnerHTML={{ __html: item.description }} className="prose max-w-none" />
                 </td>
               </tr>
