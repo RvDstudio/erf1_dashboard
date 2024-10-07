e8ff8be8242158d5a2eb24dadc085218334dcb2a
Reinier <39220977+RvDstudio@users.noreply.github.com>
2024-10-07 22:21:53 +0200

    Summary

--- src/context/OrderContext.tsx
+++ src/context/OrderContext.tsx
@@ -1,17 +1,14 @@
-"use client";
+'use client';
 // context/OrderContext.tsx
-import { createContext, useContext, useState, ReactNode } from "react";
-import { Zuivel } from "@/types"; // Assuming types are defined in a separate file
+import { createContext, useContext, useState, ReactNode } from 'react';
+import { Zuivel } from '@/types/types'; // Assuming types are defined in a separate file
 
 interface OrderContextType {
   orderData: {
     selectedProducts: Zuivel[];
     quantities: { [key: string]: number };
   };
-  setOrderData: (data: {
-    selectedProducts: Zuivel[];
-    quantities: { [key: string]: number };
-  }) => void;
+  setOrderData: (data: { selectedProducts: Zuivel[]; quantities: { [key: string]: number } }) => void;
 }
 
 const OrderContext = createContext<OrderContextType | undefined>(undefined);
@@ -22,17 +19,13 @@ export const OrderProvider = ({ children }: { children: ReactNode }) => {
     quantities: { [key: string]: number };
   }>({ selectedProducts: [], quantities: {} });
 
-  return (
-    <OrderContext.Provider value={{ orderData, setOrderData }}>
-      {children}
-    </OrderContext.Provider>
-  );
+  return <OrderContext.Provider value={{ orderData, setOrderData }}>{children}</OrderContext.Provider>;
 };
 
 export const useOrder = () => {
   const context = useContext(OrderContext);
   if (!context) {
-    throw new Error("useOrder must be used within an OrderProvider");
+    throw new Error('useOrder must be used within an OrderProvider');
   }
   return context;
 };
