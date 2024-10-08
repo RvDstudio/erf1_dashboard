ae034ad7709234955130d96f554e052e1932db00
Reinier <39220977+RvDstudio@users.noreply.github.com>
2024-10-08 14:32:23 +0200

    Summary

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@
 import { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
 import { createClient } from '@/utils/supabase/client';
+import Image from 'next/image';
 
 export default function Example() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -28,21 +29,28 @@ export default function Example() {
   };
 
   return (
-    <section className="pt-8 lg:pt-32 bg-[url('https://pagedone.io/asset/uploads/1691055810.png')] bg-center bg-cover">
+    <section className="pt-0 lg:pt-24 bg-[url('https://pagedone.io/asset/uploads/1691055810.png')] bg-center bg-cover">
       <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 relative text-center">
-        <h1 className="max-w-3xl mx-auto text-center amatic font-bold text-4xl text-[#374C69] mb-5 md:text-5xl leading-[50px]">
+        <Image
+          src="/images/thumb2.png"
+          className="flex items-center justify-center mx-auto mb-4"
+          alt="Dashboard image"
+          width={200}
+          height={150}
+        />
+        <h1 className="max-w-3xl mx-auto text-center amatic font-bold text-4xl text-[#374C69] mb-5 md:text-5xl">
           Welkom bij Erf1 zuivel & Meer
           <span className=""> Bestellingen </span>
         </h1>
-        <p className="max-w-lg mx-auto text-center text-base font-normal leading-7 text-gray-500 mb-9">
+        <p className="max-w-[600px] mx-auto text-center text-base font-normal leading-7 text-gray-500 mb-9">
           Het erf waarop onze boerderij staat is al in gebruik sinds 1432 toen in de monding van de IJsseldelta eilanden
           aaneen groeiden op de grens van zoet en zout.
         </p>
         <a
           onClick={handleClick}
-          className="cursor-pointer w-full md:w-auto mb-14 inline-flex items-center justify-center py-3 px-7 text-base font-semibold text-center text-white rounded-full bg-[#374C69] shadow-xs hover:bg-[#374C69]/90 transition-all duration-500"
+          className="cursor-pointer w-full md:w-auto mb-14 inline-flex items-center justify-center py-2 px-6 text-base  text-center text-white rounded-full bg-[#374C69] shadow-xs hover:bg-[#374C69]/90 transition-all duration-500"
         >
-          Account Aanmaken
+          Account Aanmaken / Inloggen
           <svg
             className="ml-2"
             width="20"
